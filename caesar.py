75973b97c40aae027e0284b76f19c74497be15c9
Micah Geertson <mgeertson@csu.fullerton.edu>
2016-11-15 15:27:45 -0800

    Add files via upload

---
+++ caesar.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import sys
+
+option = sys.argv[1]
+
+
+if option == '-e':
+	rot = sys.argv[2]
+	message = sys.argv[3]
+
+	for letter in message.lower():
+		shiftedLetter = chr(ord(letter) + int(rot))
+		if ord(shiftedLetter) >= 123:
+			sys.stdout.write(chr(ord(shiftedLetter) - 26))
+		else:
+			sys.stdout.write(chr(ord(shiftedLetter)))
+	print(" ")
+		
+elif option == '-d':
+	message = sys.argv[2]
+
+	for counter in range(26):
+		print("Shifting by: " + str(counter))
+		for letter in message.lower():
+			shiftedLetter = chr(ord(letter) + counter)
+			if ord(shiftedLetter) >= 123:
+				sys.stdout.write(chr(ord(shiftedLetter) - 26))
+			else:
+				sys.stdout.write(chr(ord(shiftedLetter)))
+		print("\n")
+
+else:
+	print("Invalid Option")
