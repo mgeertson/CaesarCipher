6bdcb7751c9d2d448272e9fb2d0685f8bc53ae40
Micah Geertson <mgeertson@csu.fullerton.edu>
2016-11-15 15:57:23 -0800

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -8,13 +8,17 @@ option = sys.argv[1]
 if option == '-e':
 	rot = sys.argv[2]
 	message = sys.argv[3]
+	#print(ord(message))
 
 	for letter in message.lower():
-		shiftedLetter = chr(ord(letter) + int(rot))
-		if ord(shiftedLetter) >= 123:
-			sys.stdout.write(chr(ord(shiftedLetter) - 26))
+		if ord(letter) == 32:
+			sys.stdout.write(chr(32))
 		else:
-			sys.stdout.write(chr(ord(shiftedLetter)))
+			shiftedLetter = chr(ord(letter) + int(rot))
+			if ord(shiftedLetter) >= 123:
+				sys.stdout.write(chr(ord(shiftedLetter) - 26))
+			else:
+				sys.stdout.write(chr(ord(shiftedLetter)))
 	print(" ")
 		
 elif option == '-d':
@@ -24,7 +28,9 @@ elif option == '-d':
 		print("Shifting by: " + str(counter))
 		for letter in message.lower():
 			shiftedLetter = chr(ord(letter) + counter)
-			if ord(shiftedLetter) >= 123:
+			if ord(shiftedLetter) < 64:
+				sys.stdout.write(chr(32))
+			elif ord(shiftedLetter) >= 123:
 				sys.stdout.write(chr(ord(shiftedLetter) - 26))
 			else:
 				sys.stdout.write(chr(ord(shiftedLetter)))
